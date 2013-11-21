a2468033b504070b18bf9c1e8409b940f70309ca
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2013-11-22 00:25:04 +0100

    added xervpy playbooks

---
+++ xmd/xmd_dev/roles/xmd/files/check_mk/conf.d/wato/global.mk
@@ -0,0 +1,6 @@
+# Written by WATO
+# # encoding: utf-8
+
+notification_service_subject = u'XervmonBroker: $HOSTNAME$/$SERVICEDESC$ $NOTIFICATIONTYPE$'
+notification_host_subject = u'XervmonBroker: $HOSTNAME$ - $NOTIFICATIONTYPE$'
+
